cf145c76263090e6a1fed1980d2f26d641bdcd60
Shawn Renner <shawn.renner@gmail.com>
2015-10-21 08:41:57 -0500

    work in progress

---
+++ Sandbox/ProjectEuler/PrimeUtils.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Sandbox
+{
+	public static class PrimeUtils
+	{
+
+		/// <summary>
+		/// Gets nth prime number.
+		/// First attempt at a real prime number algorithm using Sieve of Eratosthenes
+		/// </summary>
+		/// <returns>The nth prime.</returns>
+		/// <param name="n">N.</param>
+		public static long GetNthPrime(long n)
+		{
+			throw new NotImplementedException();
+			//int thisPrimePosition = 1; 	//stop when thisPrimePosition = n
+			//int currentPrime = 2;		//the value to return
+			//int maxToTry = 100000000;
+			//for (int i = 2; i <= maxToTry; i++)
+			//{
+
+			//}
+
+			//return - 1;
+		}
+
+	}
+}
+
---
+++ Sandbox/ProjectEuler/Problem0007.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Sandbox
+{
+	public class Problem0007 : IEuler
+	{
+		public Problem0007()
+		{
+		}
+
+		public string Run()
+		{
+
+			return "not done";
+		}
+	}
+}
+
